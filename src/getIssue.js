0dcbdf6ee48e03d793250f1e954528e2cc94c098
João Ougano <pedroougano@gmail.com>
2023-04-24 23:54:22 -0300

    create getIssue method

---
+++ src/getIssue.js
@@ -0,0 +1,36 @@
+const axios = require("axios");
+require("dotenv").config();
+
+const { DOMAIN_ENDPOINT, ATLASSIAN_USERNAME, ATLASSIAN_API_KEY } = process.env;
+
+const getIssue = async () => {
+  const baseUrl = `${DOMAIN_ENDPOINT}/rest/api/2/issue/CXX-2184`;
+
+  const auth = {
+      username: ATLASSIAN_USERNAME,
+      password: ATLASSIAN_API_KEY,
+    };
+    const config = {
+        method: "get",
+        url: baseUrl,
+        headers: { "Content-Type": "application/json" },
+        auth: auth,
+    };
+    
+  try {
+    const req = await axios.request(config);
+    const { assignee, status, summary } = req.data.fields;
+    const result = {
+      taskName: summary,
+      taskStatus: status.name,
+      taskAssignee: assignee.displayName,
+    };
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+getIssue();
+
+module.exports = getIssue;
